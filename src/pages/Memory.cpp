9e97d16c6ceb87b6625cccfe589a33a696a563ce
matcool <26722564+matcool@users.noreply.github.com>
2024-01-30 14:59:39 -0300

    works on android

--- src/pages/Memory.cpp
+++ src/pages/Memory.cpp
@@ -2,22 +2,74 @@
 #include "../DevTools.hpp"
 #include "../ImGui.hpp"
 #include <chrono>
+#include <algorithm>
 
 using namespace geode::prelude;
 
+#if defined(GEODE_IS_WINDOWS)
+
 #include <Windows.h>
+
+bool canReadAddr(uintptr_t addr, size_t size) {
+    return !IsBadReadPtr(reinterpret_cast<void*>(addr), size);
+}
+
+#elif defined(GEODE_IS_ANDROID)
+
+auto getReadableAddresses() {
+    using namespace std::chrono_literals;
+    static std::vector<std::pair<uintptr_t, uintptr_t>> cache;
+    // static auto lastCheck = std::chrono::high_resolution_clock::now();
+    // auto now = std::chrono::high_resolution_clock::now();
+    if (cache.empty()) {
+        cache.clear();
+        std::ifstream mappings("/proc/self/maps");
+        std::string line;
+        while (std::getline(mappings, line)) {
+            uintptr_t start, end;
+            char flags[4];
+            std::sscanf(line.c_str(), "%" PRIxPTR "-%" PRIxPTR " %4c", &start, &end, flags);
+            if (flags[0] == 'r') {
+                cache.push_back({ start, end });
+            }
+        }
+    }
+    return cache;
+} 
+
+bool canReadAddr(uintptr_t addr, size_t size) {
+    if (addr <= 0x10000) return false;
+#ifdef GEODE_IS_ANDROID64
+    // if ((addr & 0xFF00000000000000) == 0) return false;
+    addr = addr & ~(0xFF00000000000000);
+#endif
+    auto const& mappings = getReadableAddresses();
+    auto value = std::make_pair(addr, addr + size);
+    // get the largest start which is <= addr
+    auto it = std::upper_bound(mappings.rbegin(), mappings.rend(), value, [](auto const& a, auto const& b) {
+        return a.first >= b.first;
+    });
+    if (it == mappings.rend()) return false;
+    // it->first is already known to be <= addr,
+    // now just check the end
+    return value.second < it->second;
+}
+
+#endif
+
 struct SafePtr {
     uintptr_t addr = 0;
     SafePtr(uintptr_t addr) : addr(addr) {}
     SafePtr(const void* addr) : addr(reinterpret_cast<uintptr_t>(addr)) {}
 
     bool operator==(void* ptr) const { return as_ptr() == ptr; }
-    operator bool() const { return addr != 0; }
+    // breaks clang
+    // operator bool() const { return addr != 0; }
 
     void* as_ptr() const { return reinterpret_cast<void*>(addr); }
 
     bool is_safe(int size) {
-        return !IsBadReadPtr(as_ptr(), size);
+        return canReadAddr(addr, size);
     }
 
     bool read_into(void* buffer, int size) {
@@ -78,16 +130,27 @@ struct RttiInfo {
     RttiInfo(SafePtr ptr) : ptr(ptr) {}
 
     std::optional<std::string_view> class_name() {
+        // TODO: maybe cache from the typeinfo pointer?
+    #if defined(GEODE_IS_WINDOWS)
         auto vtable = ptr.read_ptr();
-        if (!vtable) return {};
+        if (!vtable.addr) return {};
         auto rttiObj = (vtable - 4).read_ptr();
-        if (!rttiObj) return {};
+        if (!rttiObj.addr) return {};
         // always 0
         auto signature = rttiObj.read<int>();
         if (signature != 0) return {};
         auto rttiDescriptor = (rttiObj + 12).read_ptr();
-        if (!rttiDescriptor) return {};
+        if (!rttiDescriptor.addr) return {};
         return demangle((rttiDescriptor + 8).read_c_str());
+    #else
+        auto vtable = ptr.read_ptr();
+        if (!vtable.addr) return {};
+        auto typeinfo = (vtable - sizeof(void*)).read_ptr();
+        if (!typeinfo.addr) return {};
+        auto typeinfoName = (typeinfo + sizeof(void*)).read_ptr();
+        if (!typeinfoName.addr) return {};
+        return typeinfoName.read_c_str();
+    #endif
     }
 };
 
@@ -112,6 +175,7 @@ void DevTools::drawMemory() {
     auto const timeNow = std::chrono::high_resolution_clock::now();
 
     // if (timeNow - lastRender < 0.5s) return;
+    // lastRender = timeNow;
 
     uintptr_t addr = 0;
     try {
@@ -121,13 +185,14 @@ void DevTools::drawMemory() {
     static std::vector<std::string> texts;
     if (changed) {
         texts.clear();
-        for (int i = 0; i < size; i += 4) {
-            SafePtr ptr = addr + i;
+        for (int offset = 0; offset < size; offset += sizeof(void*)) {
+            SafePtr ptr = addr + offset;
             RttiInfo info(ptr.read_ptr());
             auto name = info.class_name();
             if (name) {
-                texts.push_back(fmt::format("[{:03x}] {}", i, *name));
+                texts.push_back(fmt::format("[{:04x}] {}", offset, *name));
             } else {
+            #if defined(GEODE_IS_WINDOWS)
                 // scan for std::string (msvc)
                 auto size = (ptr + 16).read<size_t>();
                 auto capacity = (ptr + 20).read<size_t>();
@@ -141,13 +206,23 @@ void DevTools::drawMemory() {
                 } else {
                     data = reinterpret_cast<char*>(ptr.read_ptr().as_ptr());
                 }
+            #elif defined(GEODE_IS_ANDROID)
+                auto internalData = ptr.read_ptr();
+                if (!internalData.addr) continue;
+                auto size = (internalData - (3 * sizeof(void*))).read<size_t>();
+                auto capacity = (internalData - (2 * sizeof(void*))).read<size_t>();
+                auto refCount = (internalData - (1 * sizeof(void*))).read<int>();
+                if (size > capacity || refCount < 0) continue;
+                if (capacity > 1e8) continue;
+                char* data = reinterpret_cast<char*>(internalData.as_ptr());
+            #endif
                 if (data == nullptr || !SafePtr(data).is_safe(capacity)) continue;
                 // quick null term check
                 if (data[size] != 0) continue;
                 if (strlen(data) != size) continue;
                 auto str = std::string_view(data).substr(0, 30);
                 auto fmted = matjson::Value(std::string(str)).dump(0);
-                texts.push_back(fmt::format("[{:03x}] maybe std::string {} > {}, {}", i, size, capacity, fmted));
+                texts.push_back(fmt::format("[{:04x}] maybe std::string {} > {}, {}", offset, size, capacity, fmted));
             }
         }
     }
