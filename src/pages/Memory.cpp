8b76b2717c3a5e274578825668348faee5ec2b8c
Justin <52604018+hiimjustin000@users.noreply.github.com>
2024-07-10 14:32:39 -0400

    Update Memory.cpp

--- src/pages/Memory.cpp
+++ src/pages/Memory.cpp
@@ -325,13 +325,14 @@ void DevTools::drawMemory() {
             RttiInfo info(ptr.read_ptr());
             auto name = info.class_name();
             if (name) {
-                auto formattedPtr = fmt::ptr(*ptr.as_ptr());
+                auto voidPtr = reinterpret_cast<void**>(ptr.as_ptr());
+                auto formattedPtr = fmt::ptr(*voidPtr);
                 if (*name == "cocos2d::CCArray") {
-                    auto arr = static_cast<CCArray*>(reinterpret_cast<CCObject*>(*ptr.as_ptr()));
+                    auto arr = static_cast<CCArray*>(*reinterpret_cast<CCObject**>(voidPtr));
                     texts.push_back(fmt::format("[{:04x}] cocos2d::CCArray ({}, size {}, data {})", offset, formattedPtr, arr->data->num, fmt::ptr(arr->data->arr)));
                     textSaving.push_back(fmt::format("{:x}: a cocos2d::CCArray ({}, size {}, data {})", offset, formattedPtr, arr->data->num, fmt::ptr(arr->data->arr)));
                 } else if (*name == "cocos2d::CCDictionary") {
-                    auto dict = static_cast<CCDictionary*>(reinterpret_cast<CCObject*>(*ptr.as_ptr()));
+                    auto dict = static_cast<CCDictionary*>(*reinterpret_cast<CCObject**>(voidPtr));
                     texts.push_back(fmt::format("[{:04x}] cocos2d::CCDictionary ({}, size {}, data {})", offset, formattedPtr, HASH_COUNT(dict->m_pElements), fmt::ptr(dict->m_pElements)));
                     textSaving.push_back(fmt::format("{:x}: a cocos2d::CCDictionary ({}, size {}, data {})", offset, formattedPtr, HASH_COUNT(dict->m_pElements), fmt::ptr(dict->m_pElements)));
                 } else {
