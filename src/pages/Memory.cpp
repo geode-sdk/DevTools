d5f3ee4eb79bf66b97c85dc348843b4457812bab
Justin <52604018+hiimjustin000@users.noreply.github.com>
2024-07-10 13:39:49 -0400

    I think I'm done

--- src/pages/Memory.cpp
+++ src/pages/Memory.cpp
@@ -333,8 +333,8 @@ void DevTools::drawMemory() {
                     texts.push_back(fmt::format("[{:04x}] cocos2d::CCDictionary ({}, size {}, data {})", offset, formattedPtr, HASH_COUNT(dict->m_pElements), fmt::ptr(dict->m_pElements)));
                     textSaving.push_back(fmt::format("{:x}: a cocos2d::CCDictionary ({}, size {}, data {})", offset, formattedPtr, HASH_COUNT(dict->m_pElements), fmt::ptr(dict->m_pElements)));
                 } else {
-                    texts.push_back(fmt::format("[{:04x}] {} ({})", offset, *name));
-                    textSaving.push_back(fmt::format("{:x}: p {} ({})", offset, *name));
+                    texts.push_back(fmt::format("[{:04x}] {} ({})", offset, *name, formattedPtr));
+                    textSaving.push_back(fmt::format("{:x}: p {} ({})", offset, *name, formattedPtr));
                 }
             } else if (auto maybeStr = findStdString(ptr); maybeStr) {
                 auto str = maybeStr->substr(0, 30);
