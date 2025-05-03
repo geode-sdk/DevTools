87a65f62d1967a30433a000e8c095d138bee1cb6
Fleeym <61891787+Fleeym@users.noreply.github.com>
2025-05-03 21:03:48 +0300

    make it build

--- src/platform/utils.hpp
+++ src/platform/utils.hpp
@@ -2,9 +2,6 @@
 
 #include <string>
 #include <stdint.h>
-#ifndef GEODE_IS_WINDOWS
-#include <cxxabi.h>
-#endif
 #include <Geode/cocos/cocoa/CCObject.h>
 
 static inline std::string getNodeName(cocos2d::CCObject* node) {
