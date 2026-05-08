70a9c97432e27de7c5156c496950061baa9f828c
km7dev <100500023+Kingminer7@users.noreply.github.com>
2026-05-08 07:19:55 -0400

    Wrong branch

--- src/DevTools.cpp
+++ src/DevTools.cpp
@@ -341,6 +341,7 @@ void DevTools::setup() {
 
 #ifdef GEODE_IS_MOBILE
     ImGui::GetIO().FontGlobalScale = 2.f;
+    ImGui::GetStyle().ScrollbarSize = 60.f;
     // ImGui::GetStyle().TabBarBorderSize = 60.f;
 #endif
 }
